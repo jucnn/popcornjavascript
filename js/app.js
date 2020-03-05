b6d90c4c98dc659efc20291a8f91aa1ee7b3af51
jucnn <juliettecnn@gmail.com>
2020-03-05 14:37:15 +0100

    register form fetch

--- js/app.js
+++ js/app.js
@@ -1,92 +1,132 @@
 // Attendre chargement du DOM
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  // Déclarations
 
-    // Déclarations
+  const searchForm = document.querySelector("header form");
+  const searchLabel = document.querySelector("header form span");
+  const searchData = document.querySelector('[name="searchData"]');
+  const themoviedbURL =
+    "https://api.themoviedb.org/3/search/movie?api_key=464a7d69bc5f4b413e3ad6556a134bc2&query=";
+  const apiURL = "https://api.dwsapp.io/";
 
-    const searchForm = document.querySelector('header form');
-    const searchLabel = document.querySelector('header form span')
-    const searchData = document.querySelector('[name="searchData"]');
-    const themoviedbURL = 'https://api.themoviedb.org/3/search/movie?api_key=464a7d69bc5f4b413e3ad6556a134bc2&query=';
-    const movieList = document.querySelector('#movieList');
-    const moviePopin = document.querySelector('#moviePopin article')
+  const movieList = document.querySelector("#movieList");
+  const moviePopin = document.querySelector("#moviePopin article");
+  const registerForm = document.querySelector("#registerForm");
+  const loginForm = document.querySelector("#registerForm");
+  const userEmail = document.querySelector("#userEmail");
+  const userPassword = document.querySelector("#userPassword");
+  const userName = document.querySelector("#userName");
+  const userEmailLogin = document.querySelector("#userEmailLogin");
+  const userPasswordLogin = document.querySelector("#userPasswordLogin");
 
+  // Fonctions
 
-    // Fonctions
+  const getFormRegister = () => {
+    registerForm.addEventListener("submit", event => {
+      event.preventDefault();
 
-    const getSearchSubmit = () => {
-        searchForm.addEventListener('submit', (event) => {
-            event.preventDefault();
+      let formError = 0;
 
-            searchData.value.length > 0 ? fetchFunction(searchData.value) : displayError(searchData, 'Minimum 1 caractère couillon');
-        });
+      if (userEmail.value.length < 5) {
+        formError++;
+      }
+      if (userPassword.value.length < 5) {
+        formError++;
+      }
+      if (userName.value.length < 2) {
+        formError++;
+      }
+
+      formError != 0
+        ? console.log("Error")
+        : fetchForm("register", {
+            email: userEmail.value,
+            password: userPassword.value,
+            pseudo: userName.value
+          });
+    });
+  };
+
+  const getFormLogin = () => {
+    loginForm.addEventListener("submit", event => {
+      event.preventDefault();
+
+      fetchForm("login", {
+        email: userEmailLogin.value,
+        password: userPasswordLogin.value
+      });
+    });
+  };
+
+  const fetchForm = (endpoint, data) => {
+    let url = `${apiURL}/api/${endpoint}`;
+
+    let requestHeader = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
     };
 
-    const displayError = (tag, msg) => {
-        searchLabel.textContent = msg;
-        tag.addEventListener('focus', () => searchLabel.textContent = '');
-    };
+    fetch(url, requestHeader)
+      .then(response => {
+        return response.ok ? response.json() : "Response not OK";
+      })
+      .then(jsonData => {
+        console.log(jsonData);
+      })
+      .catch(err => console.error(err));
+  };
 
-    const fetchFunction = (keywords, index = 1) => {
-        let fetchURL = null;
+  const getSearchSubmit = () => {
+    searchForm.addEventListener("submit", event => {
+      event.preventDefault();
 
-        typeof keywords === 'number'
-        ? fetchURL = `https://api.themoviedb.org/3/movie/${keywords}?api_key=464a7d69bc5f4b413e3ad6556a134bc2`
-        : fetchURL = themoviedbURL + keywords + '&page=' + index;
+      searchData.value.length > 0
+        ? fetchFunction(searchData.value)
+        : displayError(searchData, "Minimum 1 caractère couillon");
+    });
+  };
 
-        fetch( fetchURL )
-            .then(response => {
-                return response.ok 
-                ? response.json() 
-                : 'Response not OK';
-            })
-            .then(jsonData => {
-                typeof keywords === 'number' 
-                ? displayMoviePopin(jsonData)
-                : displayMovieList(jsonData.results)
-            })
-            .catch(err => console.error(err));
-    }
+  const displayError = (tag, msg) => {
+    searchLabel.textContent = msg;
+    tag.addEventListener("focus", () => (searchLabel.textContent = ""));
+  };
 
-    //fetchFunction replace getMovieList and getMovieDetails
+  const fetchFunction = (keywords, index = 1) => {
+    let fetchURL = null;
 
-    // const getMovieList = (keywords, index = 1) => {
-    //     fetch(themoviedbURL + keywords + '&page=' + index)
-    //         .then(response => {
-    //             return response.ok ? response.json() : 'Response not OK';
-    //         })
-    //         .then(jsonData => {
-    //             displayMovieList(jsonData.results);
-    //         })
-    //         .catch(err => console.error(err));
-    // };
+    typeof keywords === "number"
+      ? (fetchURL = `https://api.themoviedb.org/3/movie/${keywords}?api_key=464a7d69bc5f4b413e3ad6556a134bc2`)
+      : (fetchURL = themoviedbURL + keywords + "&page=" + index);
 
-    // const getMovieDetails = (id) => {
-    //     // https: //api.themoviedb.org/3/movie/<ID>?api_key=<KEY>
-    //     fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=464a7d69bc5f4b413e3ad6556a134bc2`)
-    //         .then(response => {
-    //             return response.ok ? response.json() : 'Response not OK';
-    //         })
-    //         .then(jsonData => {
-    //             displayMoviePopin(jsonData);
-    //         })
-    //         .catch(err => console.error(err));
+    fetch(fetchURL)
+      .then(response => {
+        return response.ok ? response.json() : "Response not OK";
+      })
+      .then(jsonData => {
+        typeof keywords === "number"
+          ? displayMoviePopin(jsonData)
+          : displayMovieList(jsonData.results);
+      })
+      .catch(err => console.error(err));
+  };
+
+  const displayMovieList = collection => {
+    // for( let item of collection ) {
+
     // }
 
-    const displayMovieList = collection => {
+    // collection.map ( item => {
 
-        // for( let item of collection ) {
+    // })
+    searchData.value = "";
+    movieList.innerHTML = "";
 
-        // }
-
-        // collection.map ( item => {
-
-        // })
-        searchData.value = '';
-        movieList.innerHTML = '';
-
-        for (let i = 0; i < collection.length; i++) {
-            movieList.innerHTML += `
+    for (let i = 0; i < collection.length; i++) {
+      movieList.innerHTML += `
                 <article>
                     <figure>
                     <img src="https://image.tmdb.org/t/p/w300/${collection[i].poster_path}"
@@ -100,24 +140,24 @@ document.addEventListener('DOMContentLoaded', () => {
                         </div>
                     </div>
                 </article>
-            `
-        };
+            `;
+    }
 
-        getPopinLink(document.querySelectorAll('figcaption'));
+    getPopinLink(document.querySelectorAll("figcaption"));
+  };
 
-    };
+  const getPopinLink = linkCollection => {
+    for (let link of linkCollection) {
+      link.addEventListener("click", () => {
+        // + equal to Parse Int or Parse Float
+        fetchFunction(+link.getAttribute("movie-id"));
+      });
+    }
+  };
 
-    const getPopinLink = (linkCollection) => {
-        for (let link of linkCollection) {
-            link.addEventListener('click', () => {
-                fetchFunction(+link.getAttribute('movie-id'));
-            });
-        };
-    };
-
-    const displayMoviePopin = data => {
-        console.log(data);
-        moviePopin.innerHTML = `
+  const displayMoviePopin = data => {
+    console.log(data);
+    moviePopin.innerHTML = `
         <div>
             <img src="https://image.tmdb.org/t/p/w500/${data.poster_path}" alt="${data.original_title}" >
         </div>
@@ -128,23 +168,29 @@ document.addEventListener('DOMContentLoaded', () => {
             <button id="closeButton"> Close </button>
 
         </div>
-        `
+        `;
 
-        moviePopin.parentElement.classList.add('open');
-        closePopin(document.querySelector('#closeButton'))
-    }
+    moviePopin.parentElement.classList.add("open");
+    closePopin(document.querySelector("#closeButton"));
+  };
 
-    const closePopin = button => {
-        button.addEventListener('click', () => {
-            button.parentElement.parentElement.parentElement.classList.add('close');
-            setTimeout(() => {
-                button.parentElement.parentElement.parentElement.classList.remove('open');
-                button.parentElement.parentElement.parentElement.classList.remove('close');
-            }, 300)
-        })
-    }
+  const closePopin = button => {
+    button.addEventListener("click", () => {
+      button.parentElement.parentElement.parentElement.classList.add("close");
+      setTimeout(() => {
+        button.parentElement.parentElement.parentElement.classList.remove(
+          "open"
+        );
+        button.parentElement.parentElement.parentElement.classList.remove(
+          "close"
+        );
+      }, 300);
+    });
+  };
 
+  getSearchSubmit();
 
-    getSearchSubmit();
+  getFormRegister();
 
-})
+  getFormLogin();
+});
